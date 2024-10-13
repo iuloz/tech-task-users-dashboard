8fe4b4858ceb545571444c9109734d3cc24004e2
Iurii Lozhkin <lozhkinyulo@gmail.com>
2024-10-13 10:00:48 +0000

    Code formatting/linting

--- src/components/SearchAndSortBar.tsx
+++ src/components/SearchAndSortBar.tsx
@@ -22,27 +22,27 @@ function SearchAndSortBar({
   return (
     <div className={styles.searchAndSortBar}>
       <TextField
-        label="Search users by name"
-        variant="outlined"
+        label='Search users by name'
+        variant='outlined'
         value={searchQuery}
         onChange={(e) => setSearchQuery(e.target.value)}
         fullWidth
         size='small'
       />
-      <FormControl variant="outlined" fullWidth>
+      <FormControl variant='outlined' fullWidth>
         <InputLabel>Sort By</InputLabel>
         <Select
           value={sortBy}
           onChange={(e) => setSortBy(e.target.value as 'name' | 'email')}
-          label="Sort By"
+          label='Sort By'
           size='small'
         >
-          <MenuItem value="name">Name</MenuItem>
-          <MenuItem value="email">Email</MenuItem>
+          <MenuItem value='name'>Name</MenuItem>
+          <MenuItem value='email'>Email</MenuItem>
         </Select>
       </FormControl>
       <Button
-        variant="outlined" // Use "solid" for a filled button in MUI Joy
+        variant='outlined'
         onClick={() => setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc')}
         fullWidth
         size='sm'
